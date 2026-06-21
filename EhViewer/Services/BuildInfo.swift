fbd37f97f82a1859eee9571abd8c6a04e444636e
CielDevApp <CielDevApp@users.noreply.github.com>
2026-06-22 04:37:58 +0900

    chore(release): v02a-f27 (AppVersion.releaseTag 同期)

--- EhViewer/Services/BuildInfo.swift
+++ EhViewer/Services/BuildInfo.swift
@@ -2,5 +2,5 @@ import Foundation
 
 /// 自動生成: Scripts/update_build_info.sh によりビルド毎に上書きされる
 nonisolated enum BuildInfo {
-    static let tag = "2026-06-21 22:35:23 adeea16 \"feat(nhentai): タグ検索結果に言語フィルタを追加 + フィルタアイコンを E-H と統一\""
+    static let tag = "2026-06-22 04:36:33 aba8b10 \"fix(library): グリッド表示でDL進捗が表示されない不具合を修正\""
 }
