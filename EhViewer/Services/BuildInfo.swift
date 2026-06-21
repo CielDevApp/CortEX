21df31102c762b53378d668fa83119c99c754375
CielDevApp <CielDevApp@users.noreply.github.com>
2026-06-21 22:37:58 +0900

    chore(release): v02a-f26 (AppVersion.releaseTag 同期)

--- EhViewer/Services/BuildInfo.swift
+++ EhViewer/Services/BuildInfo.swift
@@ -2,5 +2,5 @@ import Foundation
 
 /// 自動生成: Scripts/update_build_info.sh によりビルド毎に上書きされる
 nonisolated enum BuildInfo {
-    static let tag = "2026-06-11 18:51:27 d39b43a+dirty \"refactor(arch): 履歴の load/save/upsert/updateLastPage を HistoryOps に集約 (B2)\""
+    static let tag = "2026-06-21 22:35:23 adeea16 \"feat(nhentai): タグ検索結果に言語フィルタを追加 + フィルタアイコンを E-H と統一\""
 }
